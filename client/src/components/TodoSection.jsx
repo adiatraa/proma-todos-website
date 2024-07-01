13f55e9f22d31f6f656d6a226b59127bdf35e0b9
adiatraa <adiatrahman31@gmail.com>
2024-07-01 14:57:04 +0700

    Add function update with press enter key when editing

--- client/src/components/TodoSection.jsx
+++ client/src/components/TodoSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ChevronDownIcon, CheckBadgeIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon, CheckBadgeIcon, ChevronRightIcon, TrashIcon } from '@heroicons/react/24/outline';
 import mainApi from '../services/mainApi';
 
 const TodoSection = ({ isAddingTask, setIsAddingTask }) => {
@@ -32,6 +32,27 @@ const TodoSection = ({ isAddingTask, setIsAddingTask }) => {
         setNewTask(newTask => ({ ...newTask, [name]: value }));
     };
 
+    const handleEditInputChange = (id, e) => {
+        const { name, value } = e.target;
+        setTodoList(prevList => prevList.map(task => task.id === id ? { ...task, [name]: value } : task));
+    }
+
+    const handleUpdateTask = (id) => {
+        const updatedTask = todoList.find(task => task.id === id);
+        mainApi
+            .put(`/todoList/${id}`, updatedTask)
+            .then((res) => {
+                setTodoList(prevList => prevList.map(task => task.id === id ? res.data : task));
+            })
+            .catch((err) => console.error(err));
+    }
+
+    const handleKeyPress = (id, e) => {
+        if (e.key === 'Enter') {
+            handleUpdateTask(id);
+        }
+    }
+
     return (
         <div>
             <button onClick={() => setSectionOpen(!sectionOpen)} className="p-5 border-b border-gray-200 flex items-center w-full">
@@ -43,19 +64,52 @@ const TodoSection = ({ isAddingTask, setIsAddingTask }) => {
                     <div key={todo.id} className="flex w-full border-b border-gray-200">
                         <div className="ml-4 flex items-center w-[505px] overflow-hidden">
                             <CheckBadgeIcon className="mr-2 h-7 w-7 text-gray-900 ml-4" aria-hidden="true" />
-                            <p>{todo.taskName}</p>
+                            <input
+                                className="w-full p-2 border-transparent focus:outline-none focus:ring-0 focus:border-transparent"
+                                name="taskName"
+                                value={todo.taskName}
+                                onChange={(e) => handleEditInputChange(todo.id, e)}
+                                onKeyPress={(e) => handleKeyPress(todo.id, e)}
+                            />
                         </div>
                         <div className="p-5 w-60 border-r border-l border-gray-200 overflow-hidden">
-                            <p>{todo.assignee}</p>
+                            <input
+                                className="w-full p-2 border-transparent focus:outline-none focus:ring-0 focus:border-transparent"
+                                name="assignee"
+                                value={todo.assignee}
+                                onChange={(e) => handleEditInputChange(todo.id, e)}
+                                onKeyPress={(e) => handleKeyPress(todo.id, e)}
+                            />
                         </div>
                         <div className="p-5 w-60 border-r border-gray-200 overflow-hidden">
-                            <p>{todo.dueDate}</p>
+                            <input
+                                className="w-full p-2 border-transparent focus:outline-none focus:ring-0 focus:border-transparent"
+                                name="dueDate"
+                                value={todo.dueDate}
+                                onChange={(e) => handleEditInputChange(todo.id, e)}
+                                onKeyPress={(e) => handleKeyPress(todo.id, e)}
+                            />
                         </div>
                         <div className="p-5 w-60 border-r border-gray-200 overflow-hidden">
-                            <p>{todo.priority}</p>
+                            <input
+                                className="w-full p-2 border-transparent focus:outline-none focus:ring-0 focus:border-transparent"
+                                name="priority"
+                                value={todo.priority}
+                                onChange={(e) => handleEditInputChange(todo.id, e)}
+                                onKeyPress={(e) => handleKeyPress(todo.id, e)}
+                            />
                         </div>
                         <div className="p-5 w-60 border-r border-gray-200 overflow-hidden">
-                            <p>{todo.status}</p>
+                            <input
+                                className="w-full p-2 border-transparent focus:outline-none focus:ring-0 focus:border-transparent"
+                                name="status"
+                                value={todo.status}
+                                onChange={(e) => handleEditInputChange(todo.id, e)}
+                                onKeyPress={(e) => handleKeyPress(todo.id, e)}
+                            />
+                        </div>
+                        <div className="p-5">
+                            <TrashIcon className="h-7 w-7 text-red-600 cursor-pointer" aria-hidden="true" onClick={() => handleDeleteTask(todo.id)} />
                         </div>
                     </div>
                 ))}
@@ -108,7 +162,7 @@ const TodoSection = ({ isAddingTask, setIsAddingTask }) => {
                             />
                         </div>
                         <div className="p-5">
-                            <button className="text-blue-600" onClick={handleSaveTask}>Save</button>
+                            <button className="text-blue-600 p-5" onClick={handleSaveTask}>Save</button>
                         </div>
                     </div>
                 )}
